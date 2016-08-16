ef8762a1ff537d278ee4397acb4559cc0216315b
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-16 15:44:06 -0700

    Added doc strings

--- linked_list.py
+++ linked_list.py
@@ -2,15 +2,15 @@
 
 
 class Node(object):
-    """ """
     def __init__(self, value, pointer=None):
+        """Create an instance of a Node."""
         self.value = value
         self.pointer = pointer
 
 
 class LinkedList(object):
-    """ """
     def __init__(self, value_list=None):
+        """Create an instance of a singly-linked list."""
         self.length = 0
         self.header = None
         if hasattr(value_list, '__iter__'):
@@ -19,26 +19,30 @@ class LinkedList(object):
         elif value_list:
             self.push(value_list)
 
-
     def __len__(self):
+        """Return the length of the linked list for the built in len."""
         return self.length
 
     def push(self, value):
+        """Add a new node to the head of the linked list."""
         temp_node = self.header
         new_node = Node(value, temp_node)
         self.header = new_node
         self.length += 1
 
     def size(self):
+        """Return the length of the linked list."""
         return len(self)
 
     def pop(self):
+        """Remove the first value off the head of the list and return it."""
         pop_node = self.header
         self.header = pop_node.pointer
         self.length -= 1
         return pop_node.value
 
     def display(self):
+        """Return a unicode string representing the list."""
         current_node = self.header
         return_str = u'('
         while current_node.pointer:
@@ -49,6 +53,7 @@ class LinkedList(object):
         return return_str
 
     def search(self, val):
+        """Return the node containing a 'val' in the list."""
         current_node = self.header
         while current_node.pointer:
             if current_node.value == val:
@@ -58,8 +63,8 @@ class LinkedList(object):
             if current_node.value == val:
                 return current_node
 
-
     def remove(self, remove_node):
+        """Remove the given node from the list."""
         if self.header == remove_node:
             self.header = remove_node.pointer
             self.length -= 1
--- test_linked_list.py
+++ test_linked_list.py
@@ -5,21 +5,26 @@ from linked_list import Node
 
 TYPE_TABLE = [(0, 0), ('a', 'a'), ([1, 2], [1, 2]), ((1, 2), (1, 2))]
 
-#Node Tests
+# Node Tests
+
+
 @pytest.mark.parametrize('init_value, result', TYPE_TABLE)
 def test_node_init_value(init_value, result):
+    """Test that the values initialize correctly."""
     test_node = Node(init_value)
     assert test_node.value == result
 
+
 @pytest.mark.parametrize('init_value, result', TYPE_TABLE)
 def test_node_init_pointer(init_value, result):
+    """Test that the pointer initializes correctly."""
     test_node = Node(init_value)
     assert test_node.pointer is None
 
+
 @pytest.mark.parametrize('init_value, result', TYPE_TABLE)
 def test_node_pointer(init_value, result):
+    """Test that the node pointer returns the correct node."""
     test_node = Node(init_value)
     second_node = Node(init_value, test_node)
     assert second_node.pointer == test_node
-
-
