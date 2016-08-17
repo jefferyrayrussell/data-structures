361b698a91e194f117e9ac73cd90820885331d33
zachrickert <zachrickert@gmail.com>
2016-08-17 00:24:46 -0700

    Fix some bugs.

--- src/stack.py
+++ src/stack.py
@@ -4,13 +4,16 @@ from linked_list import LinkedList
 
 
 class Stack(object):
-    def __init__(self, value_list):
-        self._linked_list = LinkedList(value_list)
+    def __init__(self, value_list=None):
+        if value_list:
+            self._linked_list = LinkedList(value_list)
+        else:
+            self._linked_list = LinkedList()
         self.top = self._linked_list.header
         self.size = len(self._linked_list)
 
     def is_empty(self):
-        return bool(self.size())
+        return not bool(self.size)
 
     def push(self, value):
         self._linked_list.push(value)
@@ -18,6 +21,8 @@ class Stack(object):
         self.size += 1
 
     def pop(self):
-        self._linked_list.pop()
-        self.top = self._linked_list.header
-        self.size -= 1
+        if not self.is_empty():
+            value = self._linked_list.pop()
+            self.top = self._linked_list.header
+            self.size -= 1
+            return value
