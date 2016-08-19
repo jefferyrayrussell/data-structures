dfaa1345bd8e70408f175292a6bbd3489869e558
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-19 08:44:37 -0700

    queue.py constructor and methods

--- src/queue.py
+++ src/queue.py
@@ -2,51 +2,38 @@
 from __future__ import unicode_literals
 from linked_list import LinkedList
 
+# Use the doubly-linked list as the parent just as we used the singly-linked list as the parent for the stack.
+from  double import DList
+
 
 class Queue(object):
     """Create a Queue class."""
 
     def __init__(self, value_list=None):
-        """Create an instance of a queue."""
-        if value_list:
-            self._linked_list = LinkedList(value_list)
-            self.is_empty = False
-        else:
-            self._linked_list = LinkedList()
-            self.is_empty = True
-        self.top = self._linked_list.header
-        self.size = len(self._linked_list)
-
+        """Create an instance of a queue using DList."""
+       self._dlist = DList(value_list)
+            
     def enqueue(self, value):
-        """Add value to the queue."""
-        self._linked_list.push(value)
-        self.top = self._linked_list.header
-        self.size += 1
-        self.is_empty = False
+        """Add value to the top of  the queue."""
+        return self._dlist.push(value)
 
     def denqueue(self, value): 
-        """Removes the correct item from the queue, and
-        returns its value.  Raises an error if the queue is empty."""
-        self._linked_list.push(value) self.top = self._linked_list.header
-        self.size += 1 self.is_empty = False
+        """From the bottom of the queue, remove and return value."""
+         self._dlist.shift(value)
+        return  self
 
-    # def pop(self):
-    #     """Remove the top of the stack and return it."""
-    #     if not self.is_empty:
-    #         value = self._linked_list.pop()
-    #         self.top = self._linked_list.header
-    #         self.size -= 1
-    #         if self.size == 0:
-    #             self.empty = True
-    #         return value
-
+    
+    # ?????
     def peek(self, value): 
         """Returns the next value in the queue without
         dequeueing it.  If the queue is empty, returns none."""
-        self._linked_list.push(value) self.top = self._linked_list.header
-        self.size += 1 self.is_empty = False
+        
+        if self.dlist.size = 0:
+            return None
+        else:
+            return self._dlist.next_node.value
+
 
     def size(self, value): 
         """Returns the size of the queue, 0  if  empty."""
-        self._linked_list.push(value) self.top = self._linked_list.header
-        self.size += 1 self.is_empty = False
+        return self._dlist.__len__()
