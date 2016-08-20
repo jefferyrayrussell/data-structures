8c75be442817e12f2e16703981f3add310444205
zachrickert <zachrickert@gmail.com>
2016-08-20 12:00:01 -0700

    Fix some PEP8 issues.

--- src/queue.py
+++ src/queue.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+"""Queue data structure."""
 from __future__ import unicode_literals
-from linked_list import LinkedList
 
 # Use the doubly-linked list as the parent just as we used the singly-linked list as the parent for the stack.
-from  double import DList
+from double import DList
 
 
 class Queue(object):
@@ -11,36 +11,24 @@ class Queue(object):
 
     def __init__(self, value_list=None):
         """Create an instance of a queue using DList."""
-       self._dlist = DList(value_list)
-            
+        self._dlist = DList(value_list)
+
     def enqueue(self, value):
         """Add value to the top of  the queue."""
         return self._dlist.push(value)
 
-    def dequeue(self, value): 
+    def dequeue(self, value):
         """From the bottom of the queue, remove and return value."""
-         self._dlist.shift(value)
-        return  self
+        self._dlist.shift(value)
+        return self
 
-    
-    # ?????
-    def peek(self, value): 
-        """Returns the next value in the queue without
-        dequeueing it.  If the queue is empty, returns none."""
-        
-        if self.dlist.size = 0:
-            return None
-        else:
-            return self._dlist.next_node.value
-
-    # OR????
-
+    def peek(self, value):
+        """Return the next value in the queue without dequeueing it.  If the queue is empty, returns none."""
         try:
             return self._dlist.tail_node.value
         except AttributeError:
             return None
 
-
-    def size(self, value): 
-        """Returns the size of the queue, 0  if  empty."""
+    def size(self, value):
+        """Return the size of the queue, 0  if  empty."""
         return self._dlist.__len__()
