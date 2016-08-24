18c0dd54ec8b01932c6730972359ccbb4caadb73
zachrickert <zachrickert@gmail.com>
2016-08-23 17:34:40 -0700

    Add push function.

--- heap/binheap.py
+++ heap/binheap.py
@@ -18,7 +18,6 @@ class BinHeap(object):
 
     def __init__(self, data_list=None):
         """Create an instance of a Binary Heap."""
-        self.length = 0
         self.top = None
         self.bin_heap_list = []
         try:
@@ -27,3 +26,39 @@ class BinHeap(object):
                 raise TypeError('The input is of different types; cannot create a binary heap.' + message)
         except AttributeError as message:
                 raise AttributeError('The input is not sortable; cannot create a binary heap.' + message)
+
+        data_list.reverse()
+        for idx, item in enumerate(data_list):
+            self.set_node_in_heap(item, idx)
+
+
+    def push(self, value):
+        current_node = self.set_node_in_heap(value, len(self.bin_heap_list) + 1)
+        while current_node > current_node.parent:
+            current_node.value, current_node.parent.value = current_node.parent.value, current_node.value
+            current_node = current_node.parent
+
+    def set_node_in_heap(self, value, idx):
+        if idx == 0:
+            new_node = BinNode(value)
+            self.bin_heap_list.append(new_node)
+            self.top = new_node
+        else:
+            parent_position = ((idx + 1) // 2) - 1
+            is_child_a = bool(idx % 2)
+            new_node = BinNode(value, self.bin_heap_list[parent_position])
+            if (is_child_a):
+                new_node.parent.child_a = new_node
+            else:
+                new_node.parent.child_b = new_node
+            self.bin_heap_list.append(new_node)
+
+        return new_node
+
+
+
+
+
+
+
+
